d487cb54eb03102b9a1323036fbdf847ea86331f
Jesus Berdugo <berdugopertuz@gmail.com>
2024-08-07 15:49:14 -0500

    feat: add shadcn, tailwind css and delete unnecesary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Button } from "./components/ui/button";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <main className="my-8 lg:max-w-4xl lg:mx-auto">
+      <section className="flex flex-col gap-4">
+        <h1>My Personal Library</h1>
+        <p>
+          This is a project with which I will be practicing react and also
+          integrating the api I previously built called library-api.{" "}
+        </p>
+      </section>
+      <Button className="mt-4">Test</Button>
+    </main>
+  </StrictMode>
+);
