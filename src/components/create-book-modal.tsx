6ae7a30daa426d4b6c88382a2a3e19f1fb3d7c5a
Jesus Berdugo <berdugopertuz@gmail.com>
2024-08-09 08:15:14 -0500

    feat: form for create book

--- src/components/create-book-modal.tsx
+++ src/components/create-book-modal.tsx
@@ -1,17 +1,54 @@
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { useForm } from "react-hook-form";
+import { CalendarIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AlertDialogDescription } from "@radix-ui/react-alert-dialog";
+import { BookCreateSchema, bookCreateSchema } from "@/schema/bookSchema";
 import {
   AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
-import { AlertDialogDescription } from "@radix-ui/react-alert-dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const CreateBookModal = () => {
+  const form = useForm<BookCreateSchema>({
+    resolver: zodResolver(bookCreateSchema),
+    defaultValues: {
+      price: 1,
+      title: "",
+      genre: "",
+      author: "",
+      language: "",
+      publisher: "",
+      numberOfPage: 1,
+      publicationDate: new Date(),
+    },
+  });
+
+  function onSubmit(values: BookCreateSchema) {
+    console.log(values);
+    form.reset();
+  }
+
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -24,11 +61,169 @@ const CreateBookModal = () => {
             This is a form for the creation of a book.
           </AlertDialogDescription>
         </AlertDialogHeader>
-        <h1>Form</h1>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="title" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="author"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Author</FormLabel>
+                    <FormControl>
+                      <Input placeholder="author" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="genre"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Genre</FormLabel>
+                    <FormControl>
+                      <Input placeholder="genre" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="language"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Language</FormLabel>
+                    <FormControl>
+                      <Input placeholder="language" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="publisher"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Publisher</FormLabel>
+                    <FormControl>
+                      <Input placeholder="publisher" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="numberOfPage"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Pages</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        placeholder="number of pages"
+                        {...field}
+                        onChange={(e) => field.onChange(Number(e.target.value))}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        placeholder="Price"
+                        {...field}
+                        onChange={(e) => field.onChange(Number(e.target.value))}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="publicationDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Publication date</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={"outline"}
+                            className={cn(
+                              "w-[240px] pl-3 text-left font-normal",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, "PPP")
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <div className="flex justify-end">
+              <Button
+                type="submit"
+                className="bg-green-500 duration transition-500 hover:bg-green-600"
+              >
+                Create
+              </Button>
+            </div>
+          </form>
+        </Form>
       </AlertDialogContent>
     </AlertDialog>
   );
--- src/schema/bookSchema.ts
+++ src/schema/bookSchema.ts
@@ -15,4 +15,19 @@ export const bookSchema = z.object({
   price: z.number().positive("The price cannot be a negative integer"),
 });
 
+export const bookCreateSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  author: z.string().min(1, "Author is required"),
+  publicationDate: z.date({ message: "Invalid date" }),
+  publisher: z.string().min(1, "Publisher is required"),
+  numberOfPage: z
+    .number()
+    .int()
+    .positive("Number of pages must be a positive integer"),
+  genre: z.string().min(1, "Genre is required"),
+  language: z.string().min(1, "Language is required"),
+  price: z.number().positive("The price cannot be a negative integer"),
+});
+
 export type Book = z.infer<typeof bookSchema>;
+export type BookCreateSchema = z.infer<typeof bookCreateSchema>;
