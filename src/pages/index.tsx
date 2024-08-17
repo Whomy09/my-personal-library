61629b69a2889ce4675439de3db629b8d23609f3
Jesus Berdugo <berdugopertuz@gmail.com>
2024-08-17 11:27:55 -0500

    feat: library icon in the header section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
+import { LibraryBig } from "lucide-react";
 import BooksTable from "@/components/books-table";
 import { bookColumns } from "@/components/columns";
 import { createBook, getBooks } from "@/services/book";
 import CreateBookModal from "@/components/create-book-modal";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
-export default function App() {
+const App = () => {
   const [showCreateModal, setShowCreateModal] = useState(false);
-  
+
   const queryClient = useQueryClient();
   const {
     error,
     isError,
     data: books,
-    isFetching,
+    isLoading,
   } = useQuery("books", getBooks);
 
   const mutationCreateBook = useMutation(createBook, {
@@ -26,13 +27,16 @@ export default function App() {
     <>
       <main className="my-8 lg:max-w-4xl lg:mx-auto">
         <section className="flex flex-col gap-4">
-          <h1 className="text-xl font-bold">My Personal Library</h1>
+          <header className="flex items-center gap-2">
+            <LibraryBig />
+            <h1 className="text-xl font-bold">My Personal Library</h1>
+          </header>
           <p>
             This is a project with which I will be practicing react and also
             integrating the api I previously built called library-api.{" "}
           </p>
         </section>
-        
+
         <section className="mt-4 flex flex-col gap-4">
           <div className="flex justify-end">
             <CreateBookModal
@@ -41,15 +45,17 @@ export default function App() {
               setState={setShowCreateModal}
             />
           </div>
-          {isFetching && <p>Loading books...</p>}
-          
-          {!isFetching && (
+          {isLoading && <p>Loading books...</p>}
+
+          {!isLoading && (
             <BooksTable columns={bookColumns} data={books || []} />
           )}
-          
+
           {isError && <p>Error loading books: {`${error}`}</p>}
         </section>
       </main>
     </>
   );
-}
+};
+
+export default App;
